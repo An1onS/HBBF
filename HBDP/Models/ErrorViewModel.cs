d041239e8ad43299c51fbb1b88c9126f10af56d8
Anton Sakharov <ya.antonsakharov@yandex.ru>
2019-02-14 12:21:35 +0500

    Добавьте файлы проекта.

---
+++ HBDP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HBDP.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
