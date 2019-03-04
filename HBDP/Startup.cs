a103836b506e26029d7b1b0c2aa0bd7433f090f8
Anton Sakharov <ya.antonsakharov@yandex.ru>
2019-03-04 22:27:58 +0500

    Жесткое задание культуры потока en-US

--- HBDP/Startup.cs
+++ HBDP/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,15 +26,9 @@ namespace HBDP
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.Configure<CookiePolicyOptions>(options =>
-			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded = context => true;
-				options.MinimumSameSitePolicy = SameSiteMode.None;
-			});
-
-
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+			System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");			
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);			
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
