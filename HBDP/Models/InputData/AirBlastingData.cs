5e91b8201aa954757c90f2c3ed98f8b7dcfe4dff
Anton Sakharov <ya.antonsakharov@yandex.ru>
2019-12-29 13:41:18 +0500

    AirBlasting Comments

--- HBDP/Models/InputData/AirBlastingData.cs
+++ HBDP/Models/InputData/AirBlastingData.cs
@@ -1,23 +1,44 @@
 ﻿namespace HBDP.Models
 {
+	/// <summary>
+	/// Свойства дутья
+	/// </summary>
 	public class AirBlastingData
 	{
+		/// <summary>
+		/// Температура
+		/// </summary>
 		public double Temperature { set; get; }
 		/// <summary>
-		/// Влажность дутья
+		/// Влажность
 		/// </summary>
 		public double Dampness { set; get; }
+		/// <summary>
+		/// Процент кислорода
+		/// </summary>
 		public double OxygenPercent { set; get; }
+		/// <summary>
+		/// Расход
+		/// </summary>
 		public double Consumption { set; get; }
+		/// <summary>
+		/// Доля метана
+		/// </summary>
 		public double Methane { set; get; }
+		/// <summary>
+		/// Доля этана
+		/// </summary>
 		public double Ethane { set; get; }
+		/// <summary>
+		/// Содержание CO2
+		/// </summary>
 		public double CarbonDioxide { set; get; }
 		/// <summary>
-		/// Содержание С в природном газе
+		/// Содержание С
 		/// </summary>
 		public double Carbon { set; get; }
 		/// <summary>
-		/// Содержание  Н2 в природном газе
+		/// Содержание  Н2
 		/// </summary>
 		public double Hydrogen { set; get; }
 	}
